e9c38072097b1856ea3969e096e1f9684fa95b71
Akash Jadhav <akashjadhav5975@gmail.com>
2024-11-09 14:20:41 +0530

    add origin

--- server/server.js
+++ server/server.js
@@ -40,7 +40,7 @@ const server = app.listen(port, () => {
 const io = require("socket.io")(server,{
     pingTimeout:60000,
     cors:{
-        origin:"http://localhost:3000",
+        origin:"https://chat-19ot8k1dr-akashjadhav1s-projects.vercel.app/",
     }
 })
 

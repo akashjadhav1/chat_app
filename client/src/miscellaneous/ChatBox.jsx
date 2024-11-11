eb750009492ec5099a0bbe14656c14a337234779
Akash Jadhav <akashjadhav5975@gmail.com>
2024-11-11 15:05:52 +0530

    add some responsiveness for mobile view on chat box

--- client/src/miscellaneous/ChatBox.jsx
+++ client/src/miscellaneous/ChatBox.jsx
@@ -6,8 +6,8 @@ function ChatBox({ fetchAgain, setFetchAgain }) {
   const { selectedChat } = ChatState();
 
   return (
-    <div className="flex items-center flex-col p-3 mx-2 bg-white w-full rounded-lg border-1 border-black 
-      w-[66%] md:w-3/4 lg:w-2/3 xl:w-full">
+    <div className="flex items-center flex-col p-3 mx-2 bg-white rounded-lg border-1 border-black 
+      w-[60%] md:w-3/4 lg:w-2/3 xl:w-full">
       <SingleChat fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
     </div>
   );
